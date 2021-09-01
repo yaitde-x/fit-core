8b5829151b4bab1b2504ff68d8b76accd6f24804
Eric Oliver <eo@yaitde.com>
2021-08-31 21:02:19 -0500

    new tcp listener imple

--- src/fit-runtime/Program.cs
+++ src/fit-runtime/Program.cs
@@ -8,6 +8,11 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using System.IO;
 using System.Linq;
+using TcpClient = System.Net.Sockets.TcpClient;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace fit_runtime
 {
@@ -77,7 +82,7 @@ namespace fit_runtime
             }).ToArray();
 
             var responseBuffer = JsonConvert.SerializeObject(response);
-            SendAsync(responseBuffer + "\r\n");
+            SendAsync(responseBuffer + "\r\n\r\n");
 
             // If the buffer starts with '!' the disconnect the current session
             if (request.Control == "!")
@@ -90,11 +95,101 @@ namespace fit_runtime
         }
     }
 
-    class FitnesseDebugServer : TcpServer
+    class FitnesseDebugServer
+    {
+        private readonly ILogger _logger;
+        private readonly IPAddress _address;
+        private readonly int _port;
+        private readonly FitnesseRuntime _fitnesseRuntime;
+
+        internal FitnesseDebugServer(ILogger logger, IPAddress address, int port, FitnesseRuntime fitnesseRunTime)
+        {
+            _logger = logger;
+            _address = address;
+            _port = port;
+            _fitnesseRuntime = fitnesseRunTime;
+        }
+
+        async Task HandleClient(TcpClient client, CancellationToken token)
+        {
+
+            var buffer = new byte[80000];
+            var bufferLocation = 0;
+            var clientId = Guid.NewGuid();
+            var stream = client.GetStream();
+            var eotMarkerCount = 0;
+
+            while (client.Connected && !token.IsCancellationRequested)
+            {
+                var bytesRead = await stream.ReadAsync(buffer, 0 + bufferLocation, 1);
+                if (bytesRead > 0)
+                {
+                    if (buffer[bufferLocation] == '\r' || buffer[bufferLocation] == '\n')
+                        eotMarkerCount++;
+                    else {
+                        eotMarkerCount = 0;
+                        bufferLocation++;
+                    }
+
+                    if (eotMarkerCount == 4)
+                    {
+                        var message = Encoding.UTF8.GetString(buffer, 0, bufferLocation);
+                        bufferLocation = 0;
+                        
+                        Console.WriteLine("Incoming: " + message);
+
+                        var request = JsonConvert.DeserializeObject<FitnesseRequest>(message);
+                        var response = _fitnesseRuntime.Exec(request);
+
+                        response.Globals = response.Globals.Select(sv =>
+                        {
+                            if (sv.Key.Equals("session", StringComparison.OrdinalIgnoreCase))
+                                sv.Value = clientId.ToString();
+
+                            return sv;
+                        }).ToArray();
+
+                        var responseBuffer = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(response));
+                        await stream.WriteAsync(responseBuffer);
+                    }
+                }
+            }
+        }
+
+        internal async Task Run(CancellationToken cancellationToken)
+        {
+            var listener = new TcpListener(_address, _port);
+            listener.Start();
+            cancellationToken.Register(listener.Stop);
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                try
+                {
+                    var client = await listener.AcceptTcpClientAsync();
+                    Console.WriteLine("Connection received...");
+
+                    var clientTask = HandleClient(client, cancellationToken)
+                        .ContinueWith(antecedent => client.Dispose())
+                        .ContinueWith(antecedent => _logger.LogInformation("Client disposed."));
+                }
+                catch (SocketException) when (cancellationToken.IsCancellationRequested)
+                {
+                    _logger.LogInformation("TcpListener stopped listening because cancellation was requested.");
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(new EventId(), ex, $"Error handling client: {ex.Message}");
+                }
+            }
+        }
+    }
+
+    class FitnesseDebugServerX : TcpServer
     {
         private readonly FitnesseRuntime _fitnessRuntime;
 
-        public FitnesseDebugServer(IPAddress address, int port, FitnesseRuntime fitnesseRunTime)
+        public FitnesseDebugServerX(IPAddress address, int port, FitnesseRuntime fitnesseRunTime)
             : base(address, port)
         {
             _fitnessRuntime = fitnesseRunTime;
@@ -113,7 +208,7 @@ namespace fit_runtime
 
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             JsonConvert.DefaultSettings = () => new JsonSerializerSettings
             {
@@ -144,11 +239,15 @@ namespace fit_runtime
             Console.WriteLine($"TCP server port: {port}");
             Console.WriteLine();
 
+            var loggerFactory = new LoggerFactory();
+            var logger = loggerFactory.CreateLogger("fit-debug");
+            var tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
             var runtime = new FitnesseRuntime();
-            var server = new FitnesseDebugServer(IPAddress.Any, port, runtime);
+            var server = new FitnesseDebugServer(logger, IPAddress.Any, port, runtime);
 
             Console.Write("Fitnesse Debug Server starting...");
-            server.Start();
+            var task = server.Run(token);
             Console.WriteLine("Done!");
 
             Console.WriteLine("Press Enter to stop the server or '!' to restart the server...");
@@ -160,24 +259,44 @@ namespace fit_runtime
                 if (string.IsNullOrEmpty(line))
                     break;
 
-                // Restart the server
-                if (line == "!")
-                {
-                    Console.Write("Server restarting...");
-                    server.Restart();
-                    Console.WriteLine("Done!");
-                    continue;
-                }
-
-                // Multicast admin message to all sessions
-                line = "(admin) " + line;
-                server.Multicast(line);
             }
 
             // Stop the server
             Console.Write("Server stopping...");
-            server.Stop();
+            tokenSource.Cancel();
+            await task;
             Console.WriteLine("Done!");
+        }
+    }
+
+    class Programx
+    {
+        static void Mainx(string[] args)
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 9999);
+            // we set our IP address as server's address, and we also set the port: 9999
+
+            server.Start();  // this will start the server
+
+            while (true)   //we wait for a connection
+            {
+                TcpClient client = server.AcceptTcpClient();  //if a connection exists, the server will accept it
+
+                NetworkStream ns = client.GetStream(); //networkstream is used to send/receive messages
+
+                byte[] hello = new byte[100];   //any message must be serialized (converted to byte array)
+                hello = Encoding.Default.GetBytes("hello world");  //conversion string => byte array
+
+                ns.Write(hello, 0, hello.Length);     //sending the message
+
+                while (client.Connected)  //while the client is connected, we look for incoming messages
+                {
+                    byte[] msg = new byte[1024];     //the messages arrive as byte array
+                    ns.Read(msg, 0, msg.Length);   //the same networkstream reads the message sent by the client
+                    Console.WriteLine(Encoding.UTF8.GetString(msg).Trim());
+                }
+            }
+
         }
     }
 }
